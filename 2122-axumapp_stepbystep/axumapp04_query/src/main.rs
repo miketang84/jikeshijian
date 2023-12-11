ceaf5ae43a297124efdfcf4c36bb16ec443145c1
Daogang Tang <daogangtang@gmail.com>
2023-12-11 14:04:58 +0800

    update step 04.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- 2122-axumapp_stepbystep/axumapp04_query/src/main.rs
+++ 2122-axumapp_stepbystep/axumapp04_query/src/main.rs
@@ -1,14 +1,6 @@
-use axum::{extract::Query, response::Html, routing::get, Router};
+use axum::{extract::Query, response::{Html, IntoResponse}, routing::get, Router};
 use serde::Deserialize;
-use std::net::SocketAddr;
 use tower_http::trace::TraceLayer;
-
-// #[derive(Debug, Deserialize)]
-// #[allow(dead_code)]
-// struct Params {
-//     foo: Option<i32>,
-//     bar: Option<String>,
-// }
 
 #[derive(Debug, Deserialize)]
 #[allow(dead_code)]
@@ -22,24 +14,25 @@ struct Params {
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // build our application with a route
     let app = Router::new()
         .route("/", get(handler))
-        .route("/query", get(query));
+        .route("/query", get(query))
+        .layer(TraceLayer::new_for_http());
 
-    tracing::debug!("listening on {}", addr);
-
-    axum::Server::bind(&addr)
-        .serve(app.layer(TraceLayer::new_for_http()).into_make_service())
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
 }
 
-async fn query(Query(params): Query<Params>) -> Html<&'static str> {
+async fn query(Query(params): Query<Params>) -> impl IntoResponse {
     tracing::debug!("query params {:?}", params);
     Html("<h3>Test query</h3>")
 }
