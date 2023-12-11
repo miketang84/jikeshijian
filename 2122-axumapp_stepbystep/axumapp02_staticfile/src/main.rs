4267052416ead570a80c2eca22d2ef5540c190b2
Daogang Tang <daogangtang@gmail.com>
2023-12-11 13:46:51 +0800

    update to axum 0.7, step 01~03.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- 2122-axumapp_stepbystep/axumapp02_staticfile/src/main.rs
+++ 2122-axumapp_stepbystep/axumapp02_staticfile/src/main.rs
@@ -1,49 +1,25 @@
 use axum::{response::Html, routing::get, Router};
-use std::net::SocketAddr;
 use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
-    tokio::join!(
-        serve(serve_hello(), 3000),
-        serve(serve_dir(), 3001),
-        serve(serve_dir_with_assets_fallback(), 3002),
-    );
-}
+    let serve_dir =
+        ServeDir::new("assets2").not_found_service(ServeFile::new("assets2/index.html"));
+    // build our application with a route
+    let app = Router::new()
+        .route("/foo", get(handler))
+        .nest_service("/assets", ServeDir::new("assets"))
+        .nest_service("/assets2", serve_dir.clone())
+        .fallback_service(serve_dir);
 
-async fn serve(app: Router, port: u16) {
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
-    println!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
-}
-
-fn serve_hello() -> Router {
-    // build our application with a route
-    let app = Router::new().route("/", get(handler));
-
-    app
-}
-
-fn serve_dir() -> Router {
-    // serve the file in the "assets" directory under `/assets`
-    Router::new().nest_service("/assets", ServeDir::new("assets"))
-}
-
-fn serve_dir_with_assets_fallback() -> Router {
-    // `ServeDir` allows setting a fallback if an asset is not found
-    // so with this `GET /assets/doesnt-exist.jpg` will return `index.html`
-    // rather than a 404
-    let serve_dir = ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
-
-    Router::new()
-        .route("/foo", get(|| async { "Hi from /foo" }))
-        .nest_service("/assets", serve_dir.clone())
-        .fallback_service(serve_dir)
 }
--- 2122-axumapp_stepbystep/axumapp03_addlog/src/main.rs
+++ 2122-axumapp_stepbystep/axumapp03_addlog/src/main.rs
@@ -1,5 +1,4 @@
 use axum::{response::Html, routing::get, Router};
-use std::net::SocketAddr;
 use tower_http::{
     services::{ServeDir, ServeFile},
     trace::TraceLayer,
@@ -9,47 +8,22 @@ use tower_http::{
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    tokio::join!(
-        serve(serve_hello(), 3000),
-        serve(serve_dir(), 3001),
-        serve(serve_dir_with_assets_fallback(), 3002),
-    );
-}
+    let serve_dir = ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
+    // build our application with a route
+    let app = Router::new()
+        .route("/foo", get(handler))
+        .nest_service("/assets", serve_dir.clone())
+        .fallback_service(serve_dir)
+        .layer(TraceLayer::new_for_http());
 
-async fn serve(app: Router, port: u16) {
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
-    // println!("listening on {}", addr);
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.layer(TraceLayer::new_for_http()).into_make_service())
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
-}
-
-fn serve_hello() -> Router {
-    // build our application with a route
-    let app = Router::new().route("/", get(handler));
-
-    app
-}
-
-fn serve_dir() -> Router {
-    // serve the file in the "assets" directory under `/assets`
-    Router::new().nest_service("/assets", ServeDir::new("assets"))
-}
-
-fn serve_dir_with_assets_fallback() -> Router {
-    // `ServeDir` allows setting a fallback if an asset is not found
-    // so with this `GET /assets/doesnt-exist.jpg` will return `index.html`
-    // rather than a 404
-    let serve_dir = ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
-
-    Router::new()
-        .route("/foo", get(|| async { "Hi from /foo" }))
-        .nest_service("/assets", serve_dir.clone())
-        .fallback_service(serve_dir)
 }
